1f483153d787af175e8f7ac706476bfbe3cf0311
Usuario <Usuario@DESKTOP-7K49U1J>
2017-02-14 20:49:37 +0100

    Mensaje cordial y amistoso para mi companero de proyecto.

--- HelloPartner/HelloPartner.cpp
+++ HelloPartner/HelloPartner.cpp
@@ -1,11 +1,12 @@
 // HelloPartner.cpp : Defines the entry point for the console application.
 //
-
+#include <iostream>
 #include "stdafx.h"
-
+using namespace std;
 
 int main()
 {
+	cout << "Hola David! O deberia llamarte JIMENASSOOO?" << endl;
     return 0;
 }
 
